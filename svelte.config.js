bd5238f8c37ef9fd68994a67b186ba970cb373ba
mark-casias <mark.casias@kanopi.com>
2023-03-13 15:56:36 -0600

    Netlify updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import sveltePreprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
+  kit: {
+    // default options are shown
+    adapter: adapter({
+      // if true, will create a Netlify Edge Function rather
+      // than using standard Node-based functions
+      edge: false,
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false
+    })
+  },
 
 	extensions: ['.svelte', '.md'],
 
